a2937b7917180a2bad2a627e7fc9ed4fbc172450
Faizan <mirzafaizanmail@gmail.com>
2023-04-16 22:04:05 +0530

    added ml model

--- app/src/main/java/com/parth/strokesafe/MainActivity.kt
+++ app/src/main/java/com/parth/strokesafe/MainActivity.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.parth.strokesafe.databinding.ActivityMainBinding
+import com.parth.strokesafe.ml.LiteModel
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.FloatBuffer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +22,20 @@ class MainActivity : AppCompatActivity() {
             val intent=Intent(this,signUpActivity1::class.java)
             startActivity(intent)
             finish()
+
+            val inputData = floatArrayOf(1.0f, 2.0f, 3.0f) // replace with your actual input data
+            val byteBuffer = ByteBuffer.allocateDirect(inputData.size * 4)
+            byteBuffer.asFloatBuffer().put(inputData)
+
+            val model = LiteModel.newInstance(this)
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, inputData.size), DataType.FLOAT32)
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            model.close()
         }
     }
 }
